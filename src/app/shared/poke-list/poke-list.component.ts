79724c8c0ee687cc832a34099815d2df7a7f1df1
ViniciusIshikawa <ishikawavini@hotmail.com>
2024-01-09 18:01:22 -0300

    First get from Pokemon API

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { PokeApiService } from './../../service/poke-api.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+  constructor(
+    private pokeApiService: PokeApiService
+  ) {}
 
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(res => res)
+  }
 }
