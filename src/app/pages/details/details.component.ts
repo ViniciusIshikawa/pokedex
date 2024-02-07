c322a42dbcfcfb784c9eb5564274e199caaff041
ViniciusIshikawa <ishikawavini@hotmail.com>
2024-02-07 11:35:59 -0300

    Feat: Getting pokemon's information for the detail screen

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
+//Services
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
 
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+  get pokemon() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`)
+
+    //forkJoin is used to do more than one api request at the same time. Using subscribe once.
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        console.log(res);
+      }
+    );
+  }
 }
