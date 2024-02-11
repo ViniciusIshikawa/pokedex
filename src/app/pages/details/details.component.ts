b52571a26ece3722ef6388882f5bf846258534e0
ViniciusIshikawa <ishikawavini@hotmail.com>
2024-02-11 15:10:05 -0300

    Feat: Error screen

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -15,16 +15,20 @@ export class DetailsComponent implements OnInit {
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private pokeApiService: PokeApiService
   ) {}
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon;
   }
 
-  get pokemon() {
+  get getPokemon() {
     const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`)
@@ -32,7 +36,10 @@ export class DetailsComponent implements OnInit {
     //forkJoin is used to do more than one api request at the same time. Using subscribe once.
     return forkJoin([pokemon, name]).subscribe(
       res => {
-        console.log(res);
+        this.pokemon = res;
+        this.isLoading = true;
+      }, error => {
+        this.apiError = true;
       }
     );
   }
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -11,6 +11,8 @@ export class PokeListComponent implements OnInit {
   private setAllPokemons: any;
   public getAllPokemons: any;
 
+  public apiError: boolean = false;
+
   constructor(
     private pokeApiService: PokeApiService
   ) {}
@@ -20,9 +22,10 @@ export class PokeListComponent implements OnInit {
       res => {
         this.setAllPokemons = res.results;
         this.getAllPokemons = this.setAllPokemons;
-        // console.log(res)
+      }, error => {
+        this.apiError = true;
       }
-      )
+    )
   }
 
   public getSearch(value: string) {
